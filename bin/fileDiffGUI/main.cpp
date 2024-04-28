18b36413eca7224b8fb13ccca89658f4bca7a745
lunarium <claymorealt@op.pl>
2024-04-28 23:05:30 +0200

    gui: use trailing return type

--- bin/fileDiffGUI/main.cpp
+++ bin/fileDiffGUI/main.cpp
@@ -1,3 +1,5 @@
+#include "types/aliases.hpp"
+
 #ifndef WIN32_LEAN_AND_MEAN
     #define WIN32_LEAN_AND_MEAN
 #endif /* WIN32_LEAN_AND_MEAN */
@@ -6,16 +8,20 @@
     #define _UNICODE
 #endif /* _UNICODE */
 
+#ifndef UNICODE
+    #define UNICODE
+#endif /* UNICODE */
+
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hi, HINSTANCE hprev, LPSTR cmdline, int show)
+auto CALLBACK WinMain(HINSTANCE hi, HINSTANCE hprev, LPSTR cmdline, int show) -> i32
 {
     UNREFERENCED_PARAMETER(hi);
     UNREFERENCED_PARAMETER(hprev);
     UNREFERENCED_PARAMETER(cmdline);
     UNREFERENCED_PARAMETER(show);
 
-    MessageBox(0, "hello, world!", "fileDiffGUI", MB_OK);
+    MessageBoxA(nullptr, "hello, world!", "fileDiffGUI", MB_OK);
 
     return 0;
 }
