e0dff6e2c81492f73ff6b33002f0eb17ce83537c
lunarium <claymorealt@op.pl>
2024-05-02 23:52:04 +0200

    lib: refactor and add macros

--- lib/utils/defs.h
+++ lib/utils/defs.h
@@ -20,6 +20,7 @@
 #include <stdbool.h>   // IWYU pragma: keep
 #include <stddef.h>    // IWYU pragma: keep
 #include <stdint.h>    // IWYU pragma: keep
+#include <string.h>    // IWYU pragma: keep
 
 #define FD_STRX(x) #x
 #define FD_STR(x)  FD_STRX(x)
@@ -32,16 +33,33 @@
     #define FD_EXTERN_END
 #endif /* !__cplusplus */
 
-#ifndef FD_LOCAL
-    #define FD_LOCAL static
-#endif /* !FD_LOCAL */
+#define FD_BUILD_ASSERT_OR_ZERO(cond) (sizeof(char[1 - 2 * !(cond)]) - 1)
 
-#ifndef FD_INTERNAL
-    #define FD_INTERNAL static
-#endif /* !FD_INTERNAL */
+// clang-format off
+#define FD_SWAP(a, b)                                                   \
+    do {                                                                \
+        void *_swap_ap = &(a);                                          \
+        void *_swap_bp = &(b);                                          \
+        uint8_t *_swap_buf[sizeof(a)];                                  \
+        memcpy(_swap_buf, _swap_ap, sizeof(a));                         \
+        memcpy(                                                         \
+            _swap_ap, _swap_bp,                                         \
+            sizeof(a) + FD_BUILD_ASSERT_OR_ZERO(sizeof(a == sizeof(b))) \
+        );                                                              \
+        memcpy(_swap_bp, _swap_buf, sizeof(a));                         \
+    } while (0)
+// clang-format on
 
-#ifndef FD_GLOBAL
-    #define FD_GLOBAL static
-#endif /* !FD_GLOBAL */
+#define FD_BITSIZEOF(x)         (CHAR_BIT * sizeof(x))
+
+#define FD_MAX_SIGNED_VAL(a)    (INTMAX_MAX >> (FD_BITSIZEOF(intmax_t) - FD_BITSIZEOF(a)))
+#define FD_MAX_UNSIGNED_VAL(a)  (UINTMAX_MAX >> (FD_BITSIZEOF(uintmax_t) - FD_BITSIZEOF(a)))
+
+#define FD_SIGNED_ADDOF(a, b)   ((b > FD_MAX_SIGNED_VAL(a) - (a)))
+#define FD_UNSIGNED_ADDOF(a, b) ((b > FD_MAX_UNSIGNED_VAL(a) - (a)))
+
+#define FD_LOCAL                static
+#define FD_GLOBAL               static
+#define FD_INTERNAL             static inline
 
 #endif /* !FD_LIB_DEFS */
