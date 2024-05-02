5ac3e7b0f9ee23b70e22c1eec97431dfbf2b7af5
lunarium <claymorealt@op.pl>
2024-05-02 23:53:48 +0200

    lib/win32: add path normalization

--- lib/platform/win32/win32.c
+++ lib/platform/win32/win32.c
@@ -27,8 +27,151 @@
     #include <stdlib.h>
     #include <string.h>
 
+enum FD_WIN32_PATH_TYPE
+{
+    FD_PT_ABSOLUTE,
+    FD_PT_INVALID,
+    FD_PT_NETWORK,
+    FD_PT_RELATIVE_DRIVE,
+    FD_PT_RELATIVE_ROOT,
+    FD_PT_RELATIVE,
+    FD_PT_UNC,
+};
 
-FD_EXPORT struct fdlib_file_info fdlib_win32_stat_file(char const *path)
+FD_GLOBAL char const *FD_WIN32_PATH_TYPE_STR[] = {
+    [FD_PT_ABSOLUTE]       = "absolute",                   //
+    [FD_PT_INVALID]        = "invalid",                    //
+    [FD_PT_NETWORK]        = "network",                    //
+    [FD_PT_RELATIVE_DRIVE] = "relative to current drive",  //
+    [FD_PT_RELATIVE_ROOT]  = "relative to current root",   //
+    [FD_PT_RELATIVE]       = "relative",                   //
+    [FD_PT_UNC]            = "unc/dos-device"              //
+};
+
+// Function definitions for later use
+FD_INTERNAL bool fdlib_path_is_absolute(char const *);
+FD_INTERNAL bool fdlib_path_is_network(char const *);
+FD_INTERNAL bool fdlib_path_is_relative_drive(char const *);
+FD_INTERNAL bool fdlib_path_is_relative_root(char const *);
+FD_INTERNAL bool fdlib_path_is_relative(char const *);
+FD_INTERNAL bool fdlib_path_is_unc(char const *);
+
+FD_INTERNAL bool validate_checks(int numchecks, bool checks[static numchecks])
+{
+    bool result = true;
+    for (int i = 0; i < numchecks; ++i) {
+        if (!checks[i]) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+FD_INTERNAL bool fdlib_path_is_absolute(char const *path)
+{
+    bool checks[] = { isalpha(path[0]), path[1] == ':', path[2] == '\\' };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+FD_INTERNAL bool fdlib_path_is_network(char const *path)
+{
+    bool checks[] = { path[0] == '\\', path[1] == '\\', isalpha(path[2]) };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+FD_INTERNAL bool fdlib_path_is_relative_drive(char const *path)
+{
+    bool checks[] = { isalpha(path[0]), path[1] == ':', !(path[2] == '\\') };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+FD_INTERNAL bool fdlib_path_is_relative_root(char const *path)
+{
+    bool checks[] = { path[0] == '\\', !(path[1] == '\\') };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+FD_INTERNAL bool fdlib_path_is_relative(char const *path)
+{
+    bool checks[] = {
+        !fdlib_path_is_absolute(path),
+        !fdlib_path_is_network(path),
+        !fdlib_path_is_relative_drive(path),
+        !fdlib_path_is_relative_root(path),
+        !fdlib_path_is_unc(path),
+    };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+FD_INTERNAL bool fdlib_path_is_unc(char const *path)
+{
+    bool checks[] = { path[0] == '\\', path[1] == '\\', path[2] == '?' || path[2] == '.' };
+    int numchecks = sizeof(checks) / sizeof(*checks);
+
+    return validate_checks(numchecks, checks);
+}
+
+
+char *fdlib_win32_normalize_path(char *path)
+{
+    int path_type      = FD_PT_INVALID;
+    uint_least64_t len = strlen(path);
+
+    if (fdlib_path_is_relative(path)) {
+        path_type = FD_PT_RELATIVE;
+    } else if (fdlib_path_is_relative_root(path)) {
+        path_type = FD_PT_RELATIVE_ROOT;
+    } else if (fdlib_path_is_relative_drive(path)) {
+        path_type = FD_PT_RELATIVE_DRIVE;
+    } else if (fdlib_path_is_absolute(path)) {
+        path_type = FD_PT_ABSOLUTE;
+    } else if (fdlib_path_is_network(path)) {
+        path_type = FD_PT_NETWORK;
+    } else if (fdlib_path_is_unc(path)) {
+        path_type = FD_PT_UNC;
+    } else {
+        FD_LOG_ERROR("unrecognized path format");
+        exit(-1);
+    }
+
+    FD_LOG_INFO("[path_type]:\t\'%s\'", FD_WIN32_PATH_TYPE_STR[path_type]);
+
+
+    // switch (path_type) {
+    // case FD_PT_RELATIVE: {
+
+    // } break;
+    // case FD_PT_RELATIVE_DRIVE: {
+
+    // } break;
+    // case FD_PT_ABSOLUTE: {
+
+    // } break;
+    // case FD_PT_NETWORK: {
+
+    // } break;
+    // case FD_PT_UNC: {
+
+    // } break;
+    // default: {
+    //     FD_LOG_ERROR("unrecognized path");
+    // } break;
+    // }
+}
+
+struct fdlib_file_info fdlib_win32_stat_file(char const *path)
 {
     struct fdlib_file_info fi = { 0 };
 
--- lib/platform/win32/win32.h
+++ lib/platform/win32/win32.h
@@ -17,13 +17,33 @@ FD_EXTERN_BEGIN
 
 /* ---------------------------------------------------------------------------
     Name:
+        fdlib_win32_normalize_path
+    Purpose:
+        normalizes
+    Arguments:
+        | type   | name | description       |
+        | ------ | ---- | ----------------- |
+        | char * | path | unnormalized path |
+    Return value:
+        | type   | description     |
+        | ------ | --------------- |
+        | char * | normalized path |
+   ------------------------------------------------------------------------- */
+FD_EXPORT char *fdlib_win32_normalize_path(char *path);
+
+/* ---------------------------------------------------------------------------
+    Name:
         fdlib_win32_stat_file
     Purpose:
         populates the 'fdlib_file_info' structure
     Arguments:
-        | type         | name        | description |
-        | ------------ | ----------- | ----------- |
-        | char const * | path        | filename    |
+        | type         | name | description      |
+        | ------------ | ---- | ---------------- |
+        | char const * | path | path to the file |
+    Return value:
+        | type          | description              |
+        | ------------- | ------------------------ |
+        | @(#)file_info | populated info structure |
    ------------------------------------------------------------------------- */
 FD_EXPORT struct fdlib_file_info fdlib_win32_stat_file(char const *path);
 
