5f9357907404433561ef21f93b7d0b5516c50968
lunarium <claymorealt@op.pl>
2024-05-12 19:25:34 +0200

    lib: add hash submodule to build

---
+++ lib/hash/hash_file.c
@@ -0,0 +1,8 @@
+/* ===========================================================================
+    Information:
+        lib/hash/hash_file.c   0.1.0 2024/05/12 <claymore>
+    Description:
+        file hashing, encoding and decoding
+   ========================================================================= */
+
+#include "hash_file.h"
---
+++ lib/hash/hash_file.h
@@ -0,0 +1,19 @@
+/* ===========================================================================
+    Information:
+        lib/hash/hash_file.h   0.1.0 2024/05/12 <claymore>
+    Description:
+        file hashing, encoding and decoding
+   ========================================================================= */
+
+#ifndef FD_HASH_HASH_FILE
+#define FD_HASH_HASH_FILE
+#pragma once
+
+#include "utils/defs.h"
+
+FD_EXTERN_BEGIN
+
+
+FD_EXTERN_END
+
+#endif /* !FD_HASH_HASH_FILE */
