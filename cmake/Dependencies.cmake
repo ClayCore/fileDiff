4ceca1c69d0fe0e25e2f01e93d00e53ae1226394
lunarium <claymorealt@op.pl>
2024-05-19 18:51:05 +0200

    add openssl dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,5 +1,14 @@
 include(cmake/CPM.cmake)
 
 function(dev_deps_setup)
-  # TODO
+  # This is a workaround to prevent cmake from forcing a very odd target toolchain for clang
+  # this was the only thing that actually allows this project to build and import OpenSSL
+  # at the same time.
+  #
+  # don't ask me why, or how
+  set(_OPENSSL_ROOT_PATHS PATHS "S:\\wplace\\sdk\\openssl\\x64")
+  find_package(
+    OpenSSL 3.1.2
+    REQUIRED
+  )
 endfunction()
